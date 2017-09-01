7803f016f8909dd88a04354776d33e3a386aa547
Isaac <isaacwong0108@gmail.com>
2017-09-01 21:51:02 +0800

    App Logo

--- app/src/main/java/com/feelweather/android/WeatherActivity.java
+++ app/src/main/java/com/feelweather/android/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.feelweather.android;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -21,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.feelweather.android.gson.Forecast;
 import com.feelweather.android.gson.Weather;
+import com.feelweather.android.service.AutoUpdateService;
 import com.feelweather.android.util.HttpUtil;
 import com.feelweather.android.util.Utility;
 
@@ -237,6 +239,8 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 
 }
